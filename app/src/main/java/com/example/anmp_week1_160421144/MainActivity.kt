529d8b6c6a9c79f854b5bf83d62b28d9f20cb2a9
theodorusrh <s160421144@student.ubaya.ac.id>
2024-02-19 11:43:52 +0700

    commit satu

---
+++ app/src/main/java/com/example/anmp_week1_160421144/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.anmp_week1_160421144
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ANMP_Week1_160421144"
+include(":app")
